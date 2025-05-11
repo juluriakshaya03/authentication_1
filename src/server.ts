6fbf4d4d4c20be1df3ad96afdbeefd0175bb5ff5
Naga Venkata Akshaya Juluri <nagavenkataakshaya_juluri@epam.com>
2025-05-11 20:04:07 +0530

    updated

--- src/server.ts
+++ src/server.ts
@@ -22,7 +22,7 @@ console.log('🌱 Loading env...');
 dotenv.config();
 
 console.log('🌍 Connecting to DB...');
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
   await connectDB(); // hangs here if MongoDB is unreachable
