c9e89b66d860c85f72358861245bb165f64bd8a2
Naga Venkata Akshaya Juluri <nagavenkataakshaya_juluri@epam.com>
2025-05-13 11:25:25 +0530

    updated

--- src/routes/authRoute.ts
+++ src/routes/authRoute.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { registerUser, loginUser, getUserProfile } from '../controllers/authcontroller';
 import { protect } from '../middlewares/authMiddleware';
 import { validateRegisterInput, validateLoginInput } from '../utils/validation';
+import { getCoach } from '../controllers/coaches';
 
 
 const authRouter = express.Router();
@@ -10,5 +11,6 @@ authRouter.post('/register', validateRegisterInput, registerUser);
 authRouter.post('/login', validateLoginInput, loginUser);
 authRouter.get('/profile/:id', getUserProfile);
 authRouter.get('/profile',protect, getUserProfile);
+authRouter.get('/coachInfo', getCoach);
 
 export default authRouter;
